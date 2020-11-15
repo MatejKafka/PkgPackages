61e0549e892db77b4d1edd1b0a1926511bb1915b
Matej Kafka <git@a-n.cc>
2020-11-15 04:52:43 +0100

    fixed quoting in firefox and IDEA launchers, added CLion, PhotoDemon and zig-lang

--- Firefox/78.0a1/.manifest/firefox_wrapper.cmd
+++ Firefox/78.0a1/.manifest/firefox_wrapper.cmd
@@ -1,10 +1,11 @@
-SET PACKAGE_DIR=%~dp0\..
+SET "PACKAGE_DIR=%~dp0\.."
 
-SET PROFILE=%PACKAGE_DIR%\config\profile
+SET "PROFILE=%PACKAGE_DIR%\config\profile"
 
-SET MOZ_CRASHREPORTER_DATA_DIRECTORY=%PACKAGE_DIR%\cache\crashreporter
-SET MOZ_CRASHREPORTER_EVENTS_DIRECTORY=%PACKAGE_DIR%\cache\events
-SET MOZ_CRASHREPORTER_PING_DIRECTORY=%PACKAGE_DIR%\cache\pings
+:: these don't really work now; see https://bugzilla.mozilla.org/show_bug.cgi?id=1669382
+SET "MOZ_CRASHREPORTER_DATA_DIRECTORY=%PACKAGE_DIR%\cache\crashreporter"
+SET "MOZ_CRASHREPORTER_EVENTS_DIRECTORY=%PACKAGE_DIR%\cache\events"
+SET "MOZ_CRASHREPORTER_PING_DIRECTORY=%PACKAGE_DIR%\cache\pings"
 
 cd /d "%PACKAGE_DIR%\app\"
-start "" .\firefox.exe -profile %PROFILE% %*
+start "" .\firefox.exe -profile "%PROFILE%" %*
